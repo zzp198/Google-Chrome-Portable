1cc76e6a6fffde5760215115e54e0466c0d9ff09
zzp198 <zzp198@163.com>
2023-10-23 00:11:21 +0800

    fix error on 118 version

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-**Google Chrome 118版本开始，此插件已失效，如想使用请下载117版本。**
-**https://github.com/zzp198/Google-Chrome-Portable/suites/17021789845/artifacts/972078154**
-
 屠龙者终成恶龙，再见了Edge. 此项目在前人的肩膀上，加入了自己喜欢的定制。
 
 # 功能
@@ -14,12 +11,13 @@
 - 便携设计，程序放在Chrome目录，数据和缓存放在同级目录Data\Cache下，不会在%User%/AppData中建立目录（不兼容原版数据，可以重装系统换电脑不丢数据）
 - 移除更新错误警告，移除不必要的显示（因为是绿色版没有自动更新功能）
 
+以上配置都可以在chrome++.ini中进行修改.
+
 # 获取
 
 全自动无人管理项目，每周定时拉取最新Chrome离线包，并封装为便携版。
 
-采用GitHub
-Actions自动编译发布，下载地址：[https://nightly.link/zzp198/Google-Chrome-Portable/workflows/build/main](https://nightly.link/zzp198/Google-Chrome-Portable/workflows/build/main)
+采用GitHub Actions自动编译发布，下载地址：[https://nightly.link/zzp198/Google-Chrome-Portable/workflows/build/main](https://nightly.link/zzp198/Google-Chrome-Portable/workflows/build/main)
 
 [![build status](https://github.com/zzp198/Google-Chrome-Portable/actions/workflows/build.yml/badge.svg)](https://github.com/zzp198/Google-Chrome-Portable/actions/workflows/build.yml)
 
--- run.py
+++ run.py
@@ -1,39 +1,27 @@
 import os
-from datetime import datetime
 import shutil
+from datetime import datetime
 
 import requests
 from requests_html import HTMLSession
 
-if os.path.exists('chrome.exe'):
-    os.remove('chrome.exe')
-if os.path.exists('chrome.7z'):
-    os.remove('chrome.7z')
-if os.path.exists('Chrome-bin'):
-    os.remove('Chrome-bin')
-
 url = 'https://www.iplaysoft.com/tools/chrome/'
 
 with HTMLSession() as session:
-    r = session.get('https://www.iplaysoft.com/tools/chrome/')
+    r = session.get(url)
     a = r.html.xpath('//*[@id="result_win"]/div[1]/div/div[2]/a[3]')
     href = a[0].attrs['href']
 
     r = requests.get(href, stream=True)
-    with open('chrome.exe', 'wb') as f:
+    with open('chrome.7z.exe', 'wb') as f:
         for ch in r:
             f.write(ch)
         f.close()
 
-os.system('7z.exe x chrome.exe')
-os.system('7z.exe x chrome.7z')
+os.system('7zzs x chrome.7z.exe')
+os.system('7zzs x chrome.7z')
 
-if os.path.exists('chrome.exe'):
-    os.remove('chrome.exe')
-if os.path.exists('chrome.7z'):
-    os.remove('chrome.7z')
-
-# 获得Chorme-bin,version.dll,组装到一块就可以分发了
+# 获得Chrime-bin,version.dll,组装到一块就可以分发了
 version = '0.0.0.0'
 path = 'Chrome-bin'
 for i in os.listdir(path):
@@ -45,7 +33,9 @@ print(version)
 if version == '0.0.0.0':
     exit(1)
 
-shutil.move(r'build\release\version.dll', 'Chrome-bin')
+# 获得Chrome-bin,version.dll,组装到一块就可以分发了
+shutil.move(r'version.dll', 'Chrome-bin')
+shutil.move(r'chrome++.ini', 'Chrome-bin')
 
 os.rename(r'Chrome-bin', 'Chrome')
 shutil.move(r'Chrome', 'build/release/Chrome')
